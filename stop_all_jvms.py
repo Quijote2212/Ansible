3c3227b1f4ce1923bb298c40d612d1e195d65218
Quijote2212 <jamendoza87@hotmail.com>
2023-03-28 14:57:48 -0600

    Update stop_all_jvms.py

--- stop_all_jvms.py
+++ stop_all_jvms.py
@@ -1,13 +1,5 @@
 # Import the necessary modules
 import sys
-import os
-import re
-
-# Define the function to stop a JVM
-def stop_jvm(jvm_name):
-    # Stop the JVM using AdminControl
-    print("Stopping JVM: " + jvm_name)
-    AdminControl.invoke(AdminControl.queryNames('type=JVM,name=' + jvm_name), 'stop')
 
 # Check if this script is being run with wsadmin
 if 'AdminConfig' not in globals():
@@ -19,15 +11,18 @@ cell_name = AdminControl.getCell()
 
 # Get all server names in the cell using AdminConfig
 servers = AdminConfig.list('ServerEntry').splitlines()
-server_names = [re.findall('name=([^,]+)', s)[0] for s in servers]
+server_names = [server.split('name=')[1].split(',')[0] for server in servers]
 
 # Get all JVM names in the cell using AdminControl
 jvms = []
 for server_name in server_names:
     server_id = AdminConfig.getid('/Server:' + server_name + '/')
     jvm_ids = AdminConfig.list('JavaVirtualMachine', server_id).splitlines()
-    jvms.extend([re.findall('name=([^,]+)', jvm_id)[0] for jvm_id in jvm_ids])
+    jvms.extend([jvm.split('name=')[1].split(',')[0] for jvm in jvm_ids])
 
-# Stop each JVM in the cell
+# Stop each JVM in the cell using AdminControl
 for jvm_name in jvms:
-    stop_jvm(jvm_name)
+    jvm_object_name = AdminControl.queryNames('type=JVM,name=' + jvm_name)
+    if jvm_object_name:
+        AdminControl.invoke(jvm_object_name, 'stop')
+        print("Stopped JVM: " + jvm_name)
