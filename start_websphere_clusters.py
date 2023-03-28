fb8ea591a1c12ae5f246f0a0e74c4ed5e8528084
Quijote2212 <jamendoza87@hotmail.com>
2023-03-28 17:52:51 -0600

    Create start_websphere_clusters.py

---
+++ start_websphere_clusters.py
@@ -0,0 +1,16 @@
+import java.lang.Runtime as Runtime
+
+clusters = AdminConfig.list("ServerCluster").splitlines()
+
+for cluster in clusters:
+    cluster_name = AdminConfig.showAttribute(cluster, "name")
+    print("Starting cluster: " + cluster_name)
+    servers = AdminConfig.list("ClusterMember", cluster).splitlines()
+    for server in servers:
+        server_name = AdminConfig.showAttribute(server, "memberName")
+        node_name = AdminConfig.showAttribute(server, "nodeName")
+        server_id = AdminConfig.showAttribute(server, "serverId")
+        print("Starting server: " + server_name + " on node: " + node_name)
+        process = Runtime.getRuntime().exec("wsadmin.sh -lang jython -c 'AdminControl.startServer(\"" + server_name + "\", \"" + node_name + "\", \"" + server_id + "\")'")
+        process.waitFor()
+    print("Cluster started: " + cluster_name)
