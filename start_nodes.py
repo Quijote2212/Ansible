b287187c3de22fce5e6718827caa495074fd1d9a
Quijote2212 <jamendoza87@hotmail.com>
2023-03-28 17:17:23 -0600

    Create start_nodes.py

---
+++ start_nodes.py
@@ -0,0 +1,20 @@
+# Import the necessary modules
+import sys
+
+# Check if this script is being run with wsadmin
+if 'AdminConfig' not in globals():
+    print("Error: This script should be run using wsadmin")
+    sys.exit(1)
+
+# Get the cell name using AdminConfig
+cell_name = AdminControl.getCell()
+
+# Get the list of nodes using AdminTask
+node_list = AdminTask.listNodes().splitlines()
+
+# Start each node in the cell using AdminControl
+for node_name in node_list:
+    node_object_name = AdminControl.queryNames('type=NodeAgent,node=' + node_name + ',*')
+    if node_object_name:
+        AdminControl.invoke(node_object_name, 'start')
+        print("Started node: " + node_name)
